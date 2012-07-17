8be474f34bfc66cb124a7675b847d16676f8e763
Simon Mahler <simon@darwin2.(none)>
2012-07-17 00:21:47 -0400

    solution to problem 4.

---
+++ 4.fsx
@@ -0,0 +1,22 @@
+open System
+
+//get n-th digit (right to left)
+let getNthDigit (x : int) (n : int) =
+    let ld = x % (pown 10 n) //strip front
+    (ld - ld % (pown 10 (n-1))) / (pown 10 (n-1))
+
+let digitsEqual (x : int) (len : int) (n : int) =
+    (getNthDigit x n) = (getNthDigit x (len-n+1))
+
+let isP (n : int) =
+    let len = int (Math.Log10 (float n))+1
+    let i = [1..len/2]
+    i |> List.fold (fun s i -> s && (digitsEqual n len i)) true
+
+let p = seq{100..999}    
+let q = seq{for i in p do for j in p do yield i*j}
+q |> Seq.filter (fun x -> isP x) |> Seq.max
+
+//getNthDigit 253 1
+//isP 2234322
+//digitsEqual 2 1 1
