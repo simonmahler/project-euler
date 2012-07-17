9450855bbf3d15e9d77fc0bf850885e951465d4b
Simon Mahler <simon@darwin2.(none)>
2012-07-17 00:44:42 -0400

    super slow solution to problem 5, need to use lcm

---
+++ 5.fsx
@@ -0,0 +1,8 @@
+﻿let divisors = [2..20]
+let isDivisible divs x =
+    divs |> List.fold (fun s d -> x%d=0) true 
+
+Seq.initInfinite (fun i -> i*20) 
+|> Seq.filter (isDivisible divisors)
+|> Seq.min
+
