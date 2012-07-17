4bd290e073e24427536c59fa12ff9beb923808be
Simon Mahler <simon@darwin2.(none)>
2012-07-17 19:03:09 -0400

    six finished

---
+++ 6.fsx
@@ -0,0 +1,6 @@
+﻿let sumOfSquares x =
+    x |> Seq.map (fun i -> i*i) |> Seq.sum
+let squareOfSum x =
+    x |> Seq.sum |> (fun i -> i*i)
+let s = [1..100]
+let d = (squareOfSum s) - (sumOfSquares s)
