1e3154c1442abf378a41ac1dc27ca2c2c226baed
Simon Mahler <simon@darwin2.(none)>
2012-07-17 00:45:51 -0400

    infinite seq at least

--- 5.fsx
+++ 5.fsx
@@ -1,8 +1,10 @@
 ﻿let divisors = [2..20]
 let isDivisible divs x =
+    printfn "%d" x
     divs |> List.fold (fun s d -> x%d=0) true 
 
-Seq.initInfinite (fun i -> i*20) 
+Seq.initInfinite (fun i -> i*20)
+//seq{1..20..1000000} 
 |> Seq.filter (isDivisible divisors)
 |> Seq.min
 
