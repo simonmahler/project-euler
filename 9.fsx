05ba98cb1410f3e0ac8b94742b128c2664094253
Simon Mahler <simon@darwin2.(none)>
2012-07-17 21:35:24 -0400

    worked out with simple euclids formula

---
+++ 9.fsx
@@ -0,0 +1,11 @@
+﻿let c = [1..500]
+let triplets = 
+    seq{
+        for m in c do
+            for n in [1..m] do
+                yield ((pown m 2)-(pown n 2), 2*m*n, (pown m 2)+(pown n 2))
+    }
+
+triplets 
+|> Seq.find (fun (a, b, c) -> a+b+c = 1000)
+|> (fun (a, b, c) -> a*b*c) 
