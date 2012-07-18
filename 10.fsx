60052c362a8b47dca482db53dac84ca61c706068
Simon Mahler <simon@darwin2.(none)>
2012-07-17 20:56:36 -0400

    previously submitted as 8 (wrong)

---
+++ 10.fsx
@@ -0,0 +1,8 @@
+﻿let isPrime c =
+    seq{for i in 2I::[3I..2I..(bigint (System.Math.Sqrt (float c)))] do yield i}
+    |> Seq.fold (fun s i -> s && c % i <> 0I) true
+
+(seq{3I..2000000I}
+|> Seq.filter isPrime
+|> Seq.sum) + 2I
+
